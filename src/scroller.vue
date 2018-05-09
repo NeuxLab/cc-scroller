2f14ca32a31638a313e3439c9a02ff87c3694f53
gockxml <hustgock@gmail.com>
2018-05-09 19:01:14 +0800

    add infinite loading & improve scroll performance

--- src/scroller.vue
+++ src/scroller.vue
@@ -1,9 +1,10 @@
 <template>
   <div class='scroller select-list extension-list'>
     <draggable element="ol" class='scroller-wrapper list-group' :options="{ disabled: !sortable }" :list="items" ref='wrapper' :class="{'forbid-scroll':!scrollable , 'vertical': !horizontal, 'horizontal': horizontal }" @update="sortChange">
-      <li class='scroller-item' v-for="(item, index) in items" :class="[ current === index ? active : '', landmarks[index] === 0 ? 'on-wrapper': '' ]" v-on:click='itemClick(index)'>
+      <li class='scroller-item' v-for="(item, index) in items" :class="[ current === index ? active : '', landmarks[index] === 0 ? 'on-wrapper': '' ]" v-on:click='itemClick(index)' :key="item[key]">
         <slot :index='index' :item="item"></slot>
       </li>
+      <li ref="loading"><slot name="loading" v-if="showLoading==1"></slot><slot name="loadedAll" v-if="showLoading==2"></slot><slot name="empty" v-if="showLoading==3"></slot></li>
       </draggable>
   </div>
 </template>
@@ -27,14 +28,17 @@ export default {
       wrapperLength: null,
       landmarks: [], // [-1, -1, 0, 0, 1, 1] marks
       scrollListener: null,
+      loadingTimer: null,
+      showLoading: 0
     }
   },
   props: {
-    items: null,
+    items: [],
     active: {
       type: String,
       default: "selected"
-    }
+    },
+    onFetch: Function
   },
   computed: {
     horizontal(){
@@ -48,28 +52,50 @@ export default {
     },
     sortable() {
       return this.$attrs["sortable"] !== undefined
+    },
+    performant() {
+      return !this.paging;
+    },
+    paging() {
+      return this.$attrs["paging"] !== undefined
+    },
+    key() {
+      return this.$attrs["key"]
     }
   },
   watch: {
-    items: function(newValue) {
-      this.$nextTick(this.initScrollable)
-      this.current = -1
-    },
-    current: function(newValue) {
+    current(newValue) {
       let vm = this
 
       if (newValue < 0 || newValue >= vm.items.length) return
+      var score = this.performant ? this.getScore(newValue) : this.landmarks[newValue]
 
-      if (vm.landmarks[newValue] < 0) {
+      if (score < 0) {
         vm.moveTo(newValue, true, () => { vm.$emit('current-change', newValue) })
-      } else if (vm.landmarks[newValue] > 0) {
+      } else if (score > 0) {
         vm.moveTo(newValue, false, () => { vm.$emit('current-change', newValue) })
       } else {
         vm.$emit('current-change', newValue)
       }
+    },
+    items(newValue, oldValue) {
+      if (!oldValue || !oldValue.length) {
+        this.reset();
+      }
+    },
+    showLoading(newValue) {
+      if (newValue > 0) {
+        this.$nextTick(() => {
+          this.$refs['loading'].scrollIntoView();
+        })
+      }
     }
   },
   methods: {
+    reset() {
+      this.$nextTick(this.initScrollable)
+      this.current = -1
+    },
     initScrollable() {
       let vm = this
 
@@ -88,33 +114,21 @@ export default {
       // calculate each children's position
       vm.landmark()
     },
-    landmark() {
+    landmark(index) {
       // mark whether or not an item is in the wrapper view box
       // based on landmarks
+      if (this.performant) {
+        return index;
+      }
       let vm = this
       let children = vm.$refs['wrapper'].$el.children
 
       vm.landmarks = []
       for (let index = 0; index < children.length; index++) {
-        let child = children[index]
-        // if an item in the view, then (WRAPPER_LEFT - ITEM_LEFT) > 0 and (WRAPPER_RIGHT - ITEM_RIGHT) < 0
-        // Outboundary happens when either too LEFT or too RIGHT, the multiple would always be greater than zero
-        // thus, we use (WRAPPER_LEFT - ITEM_LEFT) * (WRAPPER_RIGHT * ITEM_RIGHT) > 0 to judge whether or not an item is in the wrapper current now
-        let flag1 = vm.offset - (vm.vertical ? child.offsetTop : child.offsetLeft)
-        let flag2 = (vm.offset + vm.wrapperLength) - (vm.vertical ? child.offsetTop + child.offsetHeight : child.offsetLeft + child.offsetWidth)
-
-        // vm.landmarks[index] = flag1 * flag2 <= 0
-        if (flag1 > 0 && flag2 > 0) {
-          vm.landmarks[index] = -1
-        } else if (flag1 < 0 && flag2 < 0) {
-          vm.landmarks[index] = 1
-        } else {
-          vm.landmarks[index] = 0
-        }
+        vm.landmarks[index] = this.getScore(index);
       }
-      // console.log('update landmarks', vm.landmarks)
-
       vm.$emit('landmarks-change', vm.landmarks)
+      return vm.landmarks.indexOf(0);
     },
     moveTo(index, isFront = true, cb = null) {
       let vm = this
@@ -132,9 +146,8 @@ export default {
         vm.offset = (vm.vertical ? target.offsetTop + target.offsetHeight : target.offsetLeft + target.offsetWidth) - vm.wrapperLength
 
         // recalculate on-wrapper status
-        vm.landmark()
         // 右侧对齐标记哪些Item是需要显示后，再取第一个进行左侧对齐
-        vm.moveTo(vm.landmarks.indexOf(0), true, cb)
+        vm.moveTo(vm.landmark(index), true, cb)
       }
     },
     applyScroll(cb) {
@@ -189,7 +202,9 @@ export default {
     },
     nextCurrent(loop = false) {
       let target = this.current + 1
-      if (!loop && target > this.items.length - 1) return false;
+      if (!loop && target > this.items.length - 1) {
+        return false;
+      }
       this.current = target % this.items.length
       return true
     },
@@ -206,6 +221,46 @@ export default {
     },
     sortChange(event) {
       this.$emit("sort-change", event)
+    },
+    getScore(index) {
+      let children = this.$refs['wrapper'].$el.children
+      let child = children[index]
+      // if an item in the view, then (WRAPPER_LEFT - ITEM_LEFT) > 0 and (WRAPPER_RIGHT - ITEM_RIGHT) < 0
+      // Outboundary happens when either too LEFT or too RIGHT, the multiple would always be greater than zero
+      // thus, we use (WRAPPER_LEFT - ITEM_LEFT) * (WRAPPER_RIGHT * ITEM_RIGHT) > 0 to judge whether or not an item is in the wrapper current now
+      let flag1 = this.offset - (this.vertical ? child.offsetTop : child.offsetLeft)
+      let flag2 = (this.offset + this.wrapperLength) - (this.vertical ? child.offsetTop + child.offsetHeight : child.offsetLeft + child.offsetWidth)
+
+      // vm.landmarks[index] = flag1 * flag2 <= 0
+      if (flag1 > 0 && flag2 > 0) {
+        return -1;
+      } else if (flag1 < 0 && flag2 < 0) {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
+    fetchData(before) {
+      clearTimeout(this.loadingTimer);
+      if (this.onFetch) {
+        this.loadingTimer = setTimeout(() => {
+          before && before();
+          this.showLoading = 1;
+        }, 200)
+        this.onFetch(this.finishLoading)
+      }
+    },
+    finishLoading(end) {
+      clearTimeout(this.loadingTimer);
+      if (end) {
+        if (this.items.length) {
+          this.showLoading = 2
+        } else {
+          this.showLoading = 3
+        }
+      } else {
+        this.showLoading = 0;
+      }
     }
   },
   mounted() {
@@ -215,6 +270,9 @@ export default {
     this.scrollListener = debounce(() => {
       this.offset = (this.vertical ? wrapper.scrollTop : wrapper.scrollLeft)
       this.landmark()
+      if (this.items.length && this.getScore(this.items.length - 1) == 0) {
+        this.fetchData();
+      }
     }, 100)
     wrapper.addEventListener('scroll', this.scrollListener)
   }
@@ -230,6 +288,7 @@ export default {
 
   ol.list-group {
     max-height: none;
+    margin: 0;
   }
 
   .scroller-wrapper {
